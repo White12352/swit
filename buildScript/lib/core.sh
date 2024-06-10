e7260925905a4b2044e73abc0914d5a66ab2b3bd
White12352 <86940955+White12352@users.noreply.github.com>
2024-06-10 12:42:39 +0800

    Update core.sh

--- buildScript/lib/core.sh
+++ buildScript/lib/core.sh
@@ -2,11 +2,7 @@
 
 buildScript/lib/core/init.sh
 cd libcore
-go get github.com/sagernet/sing-box/common/geosite
-go get github.com/sagernet/sing-box/common/srs
-go get github.com/sagernet/sing-box/constant
-go get github.com/sagernet/sing-box/option
-go get github.com/sagernet/sing/common
 go mod tidy
 cd ..
+go get libcore/cmd/ruleset_generate
 buildScript/lib/core/build.sh
