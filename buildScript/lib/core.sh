9c91196855fd89e41f0a662dad824deee5a5b10c
White12352 <86940955+White12352@users.noreply.github.com>
2024-06-10 15:12:47 +0800

    Update core.sh

--- buildScript/lib/core.sh
+++ buildScript/lib/core.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 buildScript/lib/core/init.sh
-go mod tidy
-cd libcore/cmd/ruleset_generate
-cat go.mod
-go mod tidy
-cat go.mod
-cd ../..
+cd libcore
 go mod tidy
 cd ..
 buildScript/lib/core/build.sh
